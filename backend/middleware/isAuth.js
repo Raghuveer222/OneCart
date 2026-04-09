a1f95197a25857f6f03bbf9038f763a889df0200
Raghuveer222 <raghuveersinghpanwar222@gmail.com>
2026-04-09 21:45:43 +0530

    Update isAuth.js

--- backend/middleware/isAuth.js
+++ backend/middleware/isAuth.js
@@ -2,32 +2,22 @@ import jwt from "jsonwebtoken";
 
 const isAuth = (req, res, next) => {
   try {
-    const authHeader = req.headers.authorization;
-
-    if (!authHeader) {
+    const token = req.cookies.token;
+    if (!token) {
       return res
         .status(401)
         .json({ message: "Unauthorized: No token provided" });
     }
 
-
-    const token = authHeader.split(" ")[1];
-
-    if (!token) {
-      return res
-        .status(401)
-        .json({ message: "Unauthorized: Token missing" });
+    let verifyToken = jwt.verify(token, process.env.JWT_SECRET);
+    if (!verifyToken) {
+      return res.status(401).json({ message: "Unauthorized: Invalid token" });
     }
-
-
-    const verifyToken = jwt.verify(token, process.env.JWT_SECRET);
-
     req.userId = verifyToken.id;
-
     next();
   } catch (error) {
     console.error("Error in authentication middleware:", error);
-    return res.status(401).json({ message: "Unauthorized: Invalid token" });
+    return res.status(500).json({ message: "Server error" });
   }
 };
 

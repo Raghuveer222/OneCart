f521f3b199a3212342d58e5b6bb3d3157165fa83
Raghuveer222 <raghuveersinghpanwar222@gmail.com>
2026-04-09 20:44:25 +0530

    Refactor authentication middleware to use auth header

--- backend/middleware/isAuth.js
+++ backend/middleware/isAuth.js
@@ -2,22 +2,32 @@ import jwt from "jsonwebtoken";
 
 const isAuth = (req, res, next) => {
   try {
-    const token = req.cookies.token;
-    if (!token) {
+    const authHeader = req.headers.authorization;
+
+    if (!authHeader) {
       return res
         .status(401)
         .json({ message: "Unauthorized: No token provided" });
     }
 
-    let verifyToken = jwt.verify(token, process.env.JWT_SECRET);
-    if (!verifyToken) {
-      return res.status(401).json({ message: "Unauthorized: Invalid token" });
+
+    const token = authHeader.split(" ")[1];
+
+    if (!token) {
+      return res
+        .status(401)
+        .json({ message: "Unauthorized: Token missing" });
     }
+
+
+    const verifyToken = jwt.verify(token, process.env.JWT_SECRET);
+
     req.userId = verifyToken.id;
+
     next();
   } catch (error) {
     console.error("Error in authentication middleware:", error);
-    return res.status(500).json({ message: "Server error" });
+    return res.status(401).json({ message: "Unauthorized: Invalid token" });
   }
 };
 

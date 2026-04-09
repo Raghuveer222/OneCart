33d91b893f18665e76b9d696b426cbcc70f7ac2d
Raghuveer222 <raghuveersinghpanwar222@gmail.com>
2026-04-09 15:10:11 +0530

    Fix import path for AuthDataContext

--- frontend/src/pages/Login.jsx
+++ frontend/src/pages/Login.jsx
@@ -3,7 +3,7 @@ import Logo from "../assets/logo.png";
 import google from "../assets/google.png";
 import { useNavigate } from "react-router-dom";
 import { FaRegEye, FaRegEyeSlash } from "react-icons/fa";
-import { AuthDataContext } from "../context/authContext";
+import { AuthDataContext } from "../context/AuthContext";
 import axios from "axios";
 import { UserDataContext } from "../context/UserContext.jsx"; // UserContext import for getCurrentUser function
 

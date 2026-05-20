e82a4a19cb31626989a17151f6d330f73b00a745
Raghuveer222 <raghuveersinghpanwar222@gmail.com>
2026-05-20 16:38:42 +0530

    Remove console log in logout function
    Remove console log for logout result.

--- admin/src/component/Nav.jsx
+++ admin/src/component/Nav.jsx
@@ -17,7 +17,6 @@ function Nav() {
       const result = await axios.get(serverUrl + "/api/auth/logout", {
         withCredentials: true,
       });
-      console.log(result.data);
       getAdmin();
       navigate("/login");
     } catch (error) {
